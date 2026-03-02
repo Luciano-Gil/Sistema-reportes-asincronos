15cb1a0bdd227b7a906bdbffbb1fab9ccac28977
LucianoGil05 <ldgil.udc.edu.ar>
2026-03-02 01:41:48 -0300

    Fase 1: Implementación de sistema de autenticación básica y registro de personal

--- reportes/urls.py
+++ reportes/urls.py
@@ -3,10 +3,14 @@ from . import views  # importa las vistas
 from django.contrib.auth import views as auth_views #: importa todas las vistas de seguridad de Django (Login, Logout, cambio de contraseña)
 
 urlpatterns = [
-    path('', views.inicio_reporte, name='inicio'),
+    path('', views.home, name='home'),
     # Ruta de Login(usa la lógica de Django y diseño visual)
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     
     # Ruta de Logout(para cerrar la sesión de forma segura)
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+   
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -1,16 +1,40 @@
 #  función render para poder mostrar el archivo HTML(template)
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-#  tarea  creada en tasks.py para poder darle ordenes al Worker
-from .tasks import reporte_prueba
+#Importa un formulario pre-construido por Django que incluye los campos necesarios 
+# para crear un usuario (Nombre de usuario y contraseña doble) y maneja la seguridad 
+# de las claves en la base de datos PostgreSQL.
+from django.contrib.auth.forms import UserCreationForm
 
+#Es una función que "espera" a que las URLs estén cargadas antes de redirigir. Se 
+# usa en clases porque, al momento de leer el código, Django aún no conoce todas las rutas, y 
+# reverse_lazy evita errores de carga.
+from django.urls import reverse_lazy
 
-def inicio_reporte(request):
-    
-    # .delay() instruccion que  envia la tarea al buzon (Redis) de forma asíncrona
-    # Django no espera a que la tarea termine, la deja en el buzon y sigue a la siguiente linea
-    reporte_prueba.delay() 
-    
-    # se envia  una respuesta inmediata al navegador cargando el template index.html
-    # en el navegador se ve la pagina cargada al instante, mientras el Worker trabaja en segundo plano
-    return render(request, 'reportes/index.html')
+#Importa las vistas genéricas de Django. Estas son plantillas de código que ya saben cómo 
+# hacer tareas comunes, como mostrar una lista, editar un objeto o, en este caso, crear un nuevo registro.
+from django.views import generic
+
+@login_required # decorador que protege la vista,  si no se esta  logueado, manda al login
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def home(request):
+
+    return render(request, 'home.html')
+
+class SignUpView(generic.CreateView):
+    """Esta clase hereda de generic.CreateView, lo que significa que ya hereda
+      automáticamente toda la lógica para recibir datos de un formulario y guardarlos en la base de datos.
+    """
+
+    #Le dice a la vista: "Usa este formulario específico para pedir los datos al empleado".
+    form_class = UserCreationForm
+
+    #Define que, si el registro en PostgreSQL es exitoso, el sistema debe enviar 
+    # al usuario automáticamente a la página de inicio de sesión.
+    success_url = reverse_lazy('login')
+
+    #Indica cuál es el archivo HTML que debe dibujar este formulario en la pantalla del navegador.
+    template_name = 'signup.html'
